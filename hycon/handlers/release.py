809e86a8f92239d19bddbf4771dc8f419be296eb
Restyled.io <commits@restyled.io>
2021-06-28 08:41:38 +0000

    Restyled by reorder-python-imports

--- hycon/handlers/release.py
+++ hycon/handlers/release.py
@@ -1,28 +1,26 @@
 # This file is part of Hycon (Telegram Bot)
-
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import json
 import os
 
 import requests
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
-from hycon.config import UPDATES_CHANEL
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import Message
 
 from .devices import check_device
+from hycon.config import UPDATES_CHANEL
 
 DEVICES_REPO = "https://raw.githubusercontent.com/Hycon-Devices/official_devices/master"
 
